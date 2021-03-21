38078fa1477133df6a77b0d019bd1fc3dc43a2da
helder-lourenco <helderrl@yahoo.com.br>
2021-03-21 14:58:05 -0300

    Primeiros passos

---
+++ primeiroArquivo/app.js
@@ -0,0 +1,13 @@
+console.log("Ola Mundo");
+
+var nome = 'Helder';
+var sobreNome = 'Lourenco';
+console.log(nome + " " + sobreNome);
+
+
+var num = 10;
+var num1 = 20;
+function somar(a,b){
+    return a + b;
+}
+console.log(somar(num+num1));
---
+++ segundoArquivo_Calculadora/calc.js
@@ -0,0 +1,9 @@
+var SomaFunc = require("./soma");
+var SubFunc = require("./sub");
+var MultFunc = require("./mult");
+var DivFunc = require("./div");
+
+console.log(SomaFunc(1,2));
+console.log(SubFunc(3,2));
+console.log(MultFunc(1,2));
+console.log(DivFunc(4,2));
